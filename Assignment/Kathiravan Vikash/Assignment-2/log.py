bf8a5e701fb09c8a1dac46b19fe3c49c35fd6beb
KathiravanVikash <113977794+KathiravanVikash@users.noreply.github.com>
2022-11-02 21:11:22 +0530

    Create log.py

---
+++ Assignment/Kathiravan Vikash/Assignment-2/log.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 22 14:47:23 2022
+@author: M17
+"""
+
+from flask import Flask,render_template, request 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+@app.route('/login', methods =["POST"])
+def login():
+    if request.method == "POST":
+        user = request.form["nm"]
+        use = request.form["em"]
+        us = request.form["num"]
+        
+        return render_template ("index.html", b = use,y=user,c=us)
+    
+    
+if __name__==('__main__'):
+    app.run(debug = True)
